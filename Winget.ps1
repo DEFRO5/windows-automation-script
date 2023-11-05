89cd5c276790b6c06e9ed1b80a407fe377f41359
DEFRO5 <52076521+DEFRO5@users.noreply.github.com>
2023-11-06 00:45:57 +0530

    Added Misc configs

--- Winget.ps1
+++ Winget.ps1
@@ -1,6 +1,5 @@
 # Define an array of package names
 $displayoptions = @(
-    "Custom search"
     "Browsers",
     "communication tools",
     "Dev-Tools",
@@ -9,37 +8,38 @@ $displayoptions = @(
     "Multi-media tools",
     "Utilities",
     "My Config"
+    "Misc"
 )
 
 $packages = @{
-    "browserpackages"= @{
-        "All Packages"= @(
+    "browserpackages" = @{
+        "All Packages" = @(
             "LibreWolf.Librewolf",
             "Brave.Brave",
             "Google.Chrome",
             "Mozilla.Firefox"
         )
-        "LibreWolf" = "LibreWolf.Librewolf"
-        "Brave"     = "Brave.Brave"
-        "Chrome"    = "Google.Chrome"
-        "Firefox"   = "Mozilla.Firefox"
+        "LibreWolf"    = "LibreWolf.Librewolf"
+        "Brave"        = "Brave.Brave"
+        "Chrome"       = "Google.Chrome"
+        "Firefox"      = "Mozilla.Firefox"
     }
 
     "communicationpackages" = @{
-        "All Packages"      = @(
+        "All Packages" = @(
             "Discord.Discord",
             "Telegram.TelegramDesktop",
             "WhatsApp.WhatsApp",
             "Microsoft.Teams"
         )
-        "Discord"  = "Discord.Discord"
-        "Telegram" = "Telegram.TelegramDesktop"
-        "WhatsApp" = "WhatsApp.WhatsApp"
-        "Teams"    = "Microsoft.Teams"
+        "Discord"      = "Discord.Discord"
+        "Telegram"     = "Telegram.TelegramDesktop"
+        "WhatsApp"     = "WhatsApp.WhatsApp"
+        "Teams"        = "Microsoft.Teams"
     }
 
-    "devpackages"= @{
-        "All Packages"= @(
+    "devpackages" = @{
+        "All Packages" = @(
             "Git.Git",
             "Microsoft.VisualStudioCode",
             "GitHub.GitHubDesktop",
@@ -59,21 +59,21 @@ $packages = @{
         "Docker"                     = "Docker.DockerDesktop"
     }
 
-    "gamepackages"= @{
-        "All Packages"= @(
+    "gamepackages" = @{
+        "All Packages" = @(
             "EpicGames.EpicGamesLauncher",
             "Valve.Steam",
             "GOG.Galaxy",
             "RiotGames.Valorant.AP"
         )
-        "EpicGames"  = "EpicGames.EpicGamesLauncher"
-        "Steam"      = "Valve.Steam"
-        "GOG Galaxy" = "GOG.Galaxy"
-        "RiotGames"  = "RiotGames.Valorant.AP"
+        "EpicGames"    = "EpicGames.EpicGamesLauncher"
+        "Steam"        = "Valve.Steam"
+        "GOG Galaxy"   = "GOG.Galaxy"
+        "RiotGames"    = "RiotGames.Valorant.AP"
     }
 
-    "microsoftpackages"= @{
-        "All Packages"= @(
+    "microsoftpackages" = @{
+        "All Packages" = @(
             "Microsoft.VCRedist.2015+.x64",
             "Microsoft.DotNet.SDK.7",
             "Microsoft.PowerShell",
@@ -85,19 +85,19 @@ $packages = @{
         "Microsoft OneDrive"          = "Microsoft.OneDrive"
     }
 
-    "multimediapackages"= @{
-        "All Packages"= @(
+    "multimediapackages" = @{
+        "All Packages" = @(
             "Spotify.Spotify",
             "Shabinder.SpotiFlyer",
             "VideoLAN.VLC"
         )
-        "Spotify"    = "Spotify.Spotify"
-        "Spotiflyer" = "Shabinder.SpotiFlyer"
-        "VLC"        = "VideoLAN.VLC"
+        "Spotify"      = "Spotify.Spotify"
+        "Spotiflyer"   = "Shabinder.SpotiFlyer"
+        "VLC"          = "VideoLAN.VLC"
     }
 
-    "ultilitypackages"= @{
-        "All Packages"= @(
+    "ultilitypackages"  = @{
+        "All Packages"  = @(
             "7zip.7zip",
             "Surfshark.Surfshark",
             "qBittorrent.qBittorrent",
@@ -119,8 +119,8 @@ $packages = @{
         "FreeTube"        = "PrestonN.FreeTube"
     }
 
-    "myconfigpackages"= @{
-        "All Packages"= @(
+    "myconfigpackages" = @{
+        "All Packages" = @(
             "LibreWolf.Librewolf",
             "Microsoft.VisualStudioCode",
             "Discord.Discord",
@@ -147,12 +147,59 @@ $packages = @{
         "NVClean"                      = "TechPowerUp.NVCleanstall"
         "MSI Afterburner"              = "Guru3D.Afterburner"
     }
+    "Misc" = @{
+        "Custom search"                 = "Customsearch"
+        "Change Installation Directory" = "ChangeInstallationDirectory"
+    }
+
+
 }
 #GlobalVariable's
 $selection = "temp"
-$print_line =  "--------------------------------------------------------------------------------------------------"
+$print_line = "--------------------------------------------------------------------------------------------------"
+
 #InstallPackagesFunction
 Function InstallPackages {
+    if ($selectedoption -le $neededpackage.count) {
+        $confirmedoption = $allpackages[$selectedoption - 1 ]
+        switch ($confirmedoption) {
+            
+            #All packages
+            "All Packages" {
+                foreach ($packs in $neededpackage[$confirmedoption]) {
+                    winget install $packs --silent
+                    Write-Host $print_line 
+                } 
+            }
+
+            # CustomSearch
+            "Custom search" {     
+                $GetSoftname = $(Write-Host "Please, Enter the Software name: " -ForegroundColor Green -NoNewLine; Read-Host) + $(Write-Host "")
+                winget search  $GetSoftname 
+                $GetSoftId = $(Write-Host "") + $(Write-Host "Please, Enter the Software ID: " -ForegroundColor Green -NoNewLine; Read-Host) + $(Write-Host "")
+                winget install $GetSoftId  --silent
+                Write-Host $print_line 
+            }
+
+            # ChangeInstallationDirectory
+            "Change Installation Directory" {  
+                $GetSoftDirectory = $(Write-Host "Please, specify the preferred directory to install your software: " -ForegroundColor Green -NoNewLine; Read-Host) + $(Write-Host "")
+                Set-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion" -Name "ProgramFilesDir" -Value "$GetSoftDirectory"
+                $(Write-Host "Your Installation Diretory is changed to " -ForegroundColor Magenta -NoNewLine) + $(Write-Host "$GetSoftDirectory" -ForegroundColor Green ) 
+            }
+        
+            # default
+            Default {
+                winget install $neededpackage[$confirmedoption] --silent
+                Write-Host $print_line
+            }
+        }
+    }
+    DisplayPackages
+}
+
+#DisplayPackagesFunction       
+Function DisplayPackages {
     $packagearray = @(
         "browserpackages",
         "communicationpackages",
@@ -162,8 +209,24 @@ Function InstallPackages {
         "multimediapackages",
         "ultilitypackages",
         "myconfigpackages"
+        "Misc"
     )
-    $selecteduseroption = $packagearray[$selection - 2]
+    # Display package options
+    $(Write-Host "`n") + $(Write-Host "             こんにちは, Welcome to DEFALT's Windows Package Installer" -ForegroundColor Magenta) + $(Write-Host "`n")
+    for ($i = 0; $i -lt $displayoptions.Length; $i++) {
+        Write-Host ("{0}. {1}" -f ($i + 1), ($displayoptions[$i]) ) -ForegroundColor Yellow
+    }
+    Write-Host ("{0}. Exit" -f ($displayoptions.Length + 1)) -ForegroundColor Yellow
+    # Get user input
+    $temp = $(Write-Host "") + $(Write-Host "Enter the option number: " -ForegroundColor Green -NoNewLine; Read-Host) + $(Write-Host "")
+    $selection = [int]$temp
+    if ($selection -eq ($displayoptions.Length + 1)) {
+        $(Write-Host "Exiting さようなら......." -ForegroundColor Magenta -NoNewLine; ) + $(Write-Host "`n")
+        #Exit the program
+        Start-Sleep -s 3
+        Exit 
+    }
+    $selecteduseroption = $packagearray[$selection - 1]
     $neededpackage = $packages[$selecteduseroption]
     [string[]] $allpackages = $neededpackage.keys | Sort-Object
     $key_count = 0
@@ -174,58 +237,12 @@ Function InstallPackages {
     Write-Host ("{0}. Go Back" -f ($neededpackage.count + 1)) -ForegroundColor CYAN
     $Select_temp = $(Write-Host "") + $(Write-Host "Select your Package option: " -ForegroundColor Green -NoNewLine; Read-Host) + $(Write-Host "")
     $selectedoption = [int]$Select_temp
-    if ($selectedoption -le $neededpackage.count) {
-        $confirmedoption = $allpackages[$selectedoption - 1 ]
-        if ($confirmedoption -eq "All Packages") {
-            foreach ($packs in $neededpackage[$confirmedoption]) {
-                winget install $packs --silent
-                Write-Host $print_line
-            } 
-        }
-        else {
-            winget install $neededpackage[$confirmedoption] --silent
-            Write-Host $print_line
-        }
+    if ($selectedoption -eq $neededpackage.count + 1) {
+        $(Write-Host "`n") + $(Write-Host "Reverting to menu..." -ForegroundColor Cyan) 
+        DisplayPackages 
     }
-    elseif ($selectedoption -eq $neededpackage.count + 1) {
-        $(Write-Host "`n") + $(Write-Host "Reverting to menu..." -ForegroundColor Cyan) }
-    DisplayPackages
-}
-#DisplayPackagesFunction       
-Function DisplayPackages {
-    # Display package options
-    $(Write-Host "`n") + $(Write-Host "             こんにちは, Welcome to DEFALT's Windows Package Installer" -ForegroundColor Magenta) + $(Write-Host "`n")
-    for ($i = 0; $i -lt $displayoptions.Length; $i++) {
-        Write-Host ("{0}. {1}" -f ($i + 1), ($displayoptions[$i]) ) -ForegroundColor Yellow
-    }
-    Write-Host ("{0}. Exit" -f ($displayoptions.Length + 1)) -ForegroundColor Yellow
-    # Get user input
-    $temp =  $(Write-Host "") + $(Write-Host "Enter the option number: " -ForegroundColor Green -NoNewLine; Read-Host) + $(Write-Host "")
-    $selection = [int]$temp
-    #CustomSearch
-    if ($selection -eq 1)
-    {    
-        #  Write-Host `n"Type your software name:"  -ForegroundColor Green  
-         $GetSoftname =  $(Write-Host "Please, Enter the Software name: " -ForegroundColor Green -NoNewLine; Read-Host) + $(Write-Host "")
-         winget search  $GetSoftname 
-         $GetSoftId = $(Write-Host "") + $(Write-Host "Please, Enter the Software ID: " -ForegroundColor Green -NoNewLine; Read-Host) + $(Write-Host "")
-         winget install $GetSoftId  --silent
-         Write-Host $print_line
-         DisplayPackages
-    }
-    #Exit
-    elseif ($selection -eq ($displayoptions.Length + 1)) {
-         $(Write-Host "Exiting さようなら......." -ForegroundColor Magenta -NoNewLine;) + $(Write-Host "`n")
-         #Exit the program
-         Start-Sleep -s 3
-         Exit
-    }
-    elseif($selection -gt ($displayoptions.Length + 1)) {
-       $( Write-Host "Please enter a number between 1 and $($displayoptions.Count + 1) "  -ForegroundColor Cyan) + $(Write-Host "`n")
-        DisplayPackages
-    }
-    #Function call 
-    InstallPackages        
+    #Function call
+    InstallPackages   
 }
 # Call the DisplayPackages function
 DisplayPackages
